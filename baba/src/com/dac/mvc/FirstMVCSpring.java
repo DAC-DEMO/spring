edfe4276dd36c781e4ebc8b8c388b9412b54d3c1
santosh <santosh.ece06@gmail.com>
2017-07-23 09:37:04 +0530

    file upload using spring.

--- baba/src/com/dac/mvc/FirstMVCSpring.java
+++ baba/src/com/dac/mvc/FirstMVCSpring.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.dac.dao.PostDao;
 import com.dac.model.PostClass;
@@ -55,6 +59,11 @@ public class FirstMVCSpring {
 	public PostClass createPost(@RequestBody PostClass post){
 		postDao.insertPost(post);
 		return post;
+	}
+	
+	@PostMapping("upload")
+	public String imageUpload(@RequestParam CommonsMultipartFile file, HttpServletRequest request){
+		return "Hello world";
 	}
 }
 
